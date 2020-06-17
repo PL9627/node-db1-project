f91d7dbcf0c206b28746028bec317d2395c7901c
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-06-17 16:48:10 -0700

    added try catch skeletons to accounts-router.js

--- routers/accounts-router.js
+++ routers/accounts-router.js
@@ -5,22 +5,42 @@ const db = require("../data/dbConfig");
 const router = express.Router();
 
 router.get ("/", async (req, res, next) => {
+    try {
 
+    } catch (err) {
+        next(err)
+    }
 })
 
 router.get("/:id", async (req, res, next) => {
+    try {
 
+    } catch (err) {
+        next(err)
+    }
 })
 
 router.post("/", async (req, res, next) => {
+    try {
 
+    } catch (err) {
+        next(err)
+    }
 })
 
 router.put("/:id", async (req, res, next) => {
+    try {
 
+    } catch (err) {
+        next(err)
+    }
 })
 
 router.delete("/:id", async (req, res, next) => {
-    
+    try {
+
+    } catch (err) {
+        next(err)
+    }
 })
 module.exports = router;
