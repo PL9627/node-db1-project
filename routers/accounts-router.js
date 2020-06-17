008cc85fc4d781fdeae6e79897e8310e46c76bf7
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-06-17 16:50:45 -0700

    finished GET / call

--- routers/accounts-router.js
+++ routers/accounts-router.js
@@ -4,43 +4,41 @@ const db = require("../data/dbConfig");
 
 const router = express.Router();
 
-router.get ("/", async (req, res, next) => {
-    try {
+router.get("/", async (req, res, next) => {
+  try {
+    const accounts = await db.select("*").from("accounts");
 
-    } catch (err) {
-        next(err)
-    }
-})
+    res.json(accounts);
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.get("/:id", async (req, res, next) => {
-    try {
-
-    } catch (err) {
-        next(err)
-    }
-})
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.post("/", async (req, res, next) => {
-    try {
-
-    } catch (err) {
-        next(err)
-    }
-})
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.put("/:id", async (req, res, next) => {
-    try {
-
-    } catch (err) {
-        next(err)
-    }
-})
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
 
 router.delete("/:id", async (req, res, next) => {
-    try {
-
-    } catch (err) {
-        next(err)
-    }
-})
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
 module.exports = router;
