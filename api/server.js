a923b77e7179750d9a0275cdcd8fbce230a57b3e
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-06-17 16:46:50 -0700

    added err DRY code in server.js

--- api/server.js
+++ api/server.js
@@ -6,4 +6,11 @@ const server = express();
 
 server.use(express.json());
 
+server.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({
+    message: "Something went wrong",
+  });
+});
+
 module.exports = server;
